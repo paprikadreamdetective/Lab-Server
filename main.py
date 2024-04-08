b8bc6d9e119647c83499fc1df5dfb2866f4a618e
paprikadreamdetective <t4v0sg@gmail.com>
2024-04-07 20:54:31 -0600

    new account asign

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from factory.socketf import SocketFactory
 
-
+# Hola mundo
 def main():
     multicast_socket_receiver = SocketFactory.create_multicast_socket('224.10.10.10', 10000)
     while True:
